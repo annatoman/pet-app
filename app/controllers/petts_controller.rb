6e30b2e6e281c0f4f5b70caee1fd1badd04f925f
Anna <annamtoman@gmail.com>
2023-03-20 15:18:00 -0500

    adds new/create actions

--- app/controllers/petts_controller.rb
+++ app/controllers/petts_controller.rb
@@ -6,4 +6,18 @@ class PettsController < ApplicationController
     def show
         @pett = Pett.find_by(id: params[:id])
     end
+
+    def new
+        @pett = Pett.new
+        render :new
+    end
+
+    def create
+        @pett = Pett.create(
+            name: params[:pett][:name],
+            breed: params[:pett][:breed],
+            image: params[:pett][:image]
+        )
+        redirect_to "/petts"
+    end
 end
